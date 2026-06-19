33cb74811f85fa85f4607eeb673650de4fd5a501
James Friedenberg <jamesfriedenberg@gmail.com>
2026-06-19 16:57:21 -0400

    fix: expose Swagger in all environments; remove redundant deploy step

--- src/JeemzuApi/Program.cs
+++ src/JeemzuApi/Program.cs
@@ -60,11 +60,8 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("JeemzuFrontend");
